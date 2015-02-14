5799891ae635e051caf121da03ba04c0a943693a
raphaelgh <raphaelgh@hotmail.com>
2015-02-14 22:33:23 +0800

    pass test case: person fight with person

--- testcase/GameTest.java
+++ testcase/GameTest.java
@@ -52,7 +52,7 @@ public class GameTest {
 //	}
 	
 	@Test
-	public void should_player_soldier_use_weapon_fight_with_player_person(){
+	public void should_soldier_use_weapon_fight_with_person(){
 		Weapon weapon = new Weapon("优质木棒", 10);
 		Soldier playerA = new Soldier("李四",40,100,weapon,20);
 		Player playerB = new Player("张三", 30, 100);
@@ -63,5 +63,18 @@ public class GameTest {
 		verify(out).println("战士李四用优质木棒攻击了普通人张三,张三受到50点伤害,张三剩余生命:0");
 		verify(out).println("张三被打败了");
 	}
-
+	
+	@Test
+	public void should_person_fight_with_person(){
+		Player playerA = new Player("李四",40,100);
+		Player playerB = new Player("张三", 30, 100);
+		Game game = new Game(playerA, playerB, out);
+		game.start();
+		verify(out).println("普通人李四攻击了普通人张三,张三受到40点伤害,张三剩余生命:60");
+		verify(out).println("普通人张三攻击了普通人李四,李四受到30点伤害,李四剩余生命:70");
+		verify(out).println("普通人李四攻击了普通人张三,张三受到40点伤害,张三剩余生命:20");
+		verify(out).println("普通人张三攻击了普通人李四,李四受到30点伤害,李四剩余生命:40");
+		verify(out).println("普通人李四攻击了普通人张三,张三受到40点伤害,张三剩余生命:-20");
+		verify(out).println("张三被打败了");
+	}
 }
